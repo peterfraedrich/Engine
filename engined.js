5bbd2321bbd15479e48030a7f535b756c0fb41e0
Peter Fraedrich <peter.fraedrich@hexapp.net>
2016-01-21 20:32:54 -0500

    pretty much the entire thing

--- engined.js
+++ engined.js
@@ -3,8 +3,6 @@
 ////////////////////////////////////////////////////////// DEPS
 var application_root = __dirname,
     express = require('express'),
-    serveStatic = require('serve-static')
-    exec = require('child_process').exec,
     methodOverride = require('method-override'),
     errorhandler = require('errorhandler'),
     bodyParser = require('body-parser')
@@ -12,10 +10,12 @@ var application_root = __dirname,
     sys = require('sys'),
     ini = require('ini'),
     fs = require('fs'),
+    NginxConfFile = require('nginx-conf').NginxConfFile,
+    uuid = require('node-uuid')
 
 ////////////////////////////////////////////////////////// CLASS INSTANTIATION & VARS
 var app = express();
-var config = ini.parse(fs.readFileSync('engine.conf', 'utf-8'))
+var gc = ini.parse(fs.readFileSync('engine.conf', 'utf-8'))
 
 
 ////////////////////////////////////////////////////////// ALLOW XSS / CORS
@@ -28,7 +28,7 @@ var allowCrossDomain = function(req, res, next) {
 
       // intercept OPTIONS method
       if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
+        res.send(200);
       }
       else {
         next();
@@ -40,3 +40,290 @@ var allowCrossDomain = function(req, res, next) {
     app.use(methodOverride());
     app.use(errorhandler());
     app.use(express.static(path.join(application_root, "public")));
+
+////////////////////////////////////////////////////////// PRIVATE LOGIC
+
+var firstrun = function(callback) {
+    if (gc.engined.firstrun == 1 || gc.engined.firstrun == '1') {
+        generate_auth_key(function (err, key) {
+            gc.global.auth_key = key
+            gc.engined.firstrun = 0
+            fs.writeFileSync('engine.conf', ini.stringify(gc))
+            log('Completed first-run tasks. Auth key is ' + key, '000', ' ', function (err) {if (err) { console.log(err) }})
+            callback(null)
+        })
+    } else {
+        callback(null)
+    }
+}
+
+var generate_auth_key = function (callback) {
+    authkey = uuid.v4().toString().split('-')
+    key = authkey[0] + authkey[1] + authkey[2] + authkey[3] + authkey[4]
+    callback(null, key.substring(10,19))
+}
+
+var log = function (message, code, errtext, callback) {
+    var msg = {}
+    msg.timestamp = new Date().toString()
+    msg.code = code
+    msg.message = message
+    msg.err = errtext
+    fs.appendFile(gc.engined.logpath, JSON.stringify(msg) + '\n', encoding='utf-8', function (err) {
+        if (err) {
+            console.log(err)
+            callback(err)
+        } else {
+            callback(null)
+        }
+    })
+}
+
+var _err = function (req, err, callback) {
+    if (req.query.robot != undefined) {
+        console.log(err)
+        log(err.message, '999', ' ', function (err) { if (err) { console.log(err)}})
+        callback(null, 503)
+    } else {
+        console.log(err)
+        log(err.message, '999', ' ', function (err) { if (err) { console.log(err)}})
+        callback(null, err.message)
+    }
+}
+
+var check_auth_key = function (key, callback) {
+    if (key != gc.global.auth_key) {
+        log('Attempted authentication with auth key ' + key, '666', key, function (err) { if (err) { console.log(err)}})
+        callback(new Error("Hey, you can't do that, man! You don't have the right authorization key."))
+    } else {
+        callback(null)
+    }
+}
+
+var get_nginx_config = function (callback) {
+    NginxConfFile.create(gc.global.nginx_conf, function (err, config) {
+        if (err) {
+            console.log(err)
+            callback(err)
+        } else {
+            callback(null, config.nginx)
+        }
+    })
+}
+
+var pool_add = function (stream, server, callback) {
+    NginxConfFile.create(gc.global.nginx_conf, function (err, config) {
+        if (err) {
+            log('Error in pool_add', '200', err, function (err) { if (err) { console.log(err)}})
+            console.log(err)
+            callback(err)
+        }
+        for (i=0; i < config.nginx.http.upstream.length; i++) {
+            if (config.nginx.http.upstream[i]._value == stream) {
+                // check to see if its already in the stream
+                for (v = 0; v < config.nginx.http.upstream[i].server.length; v++) {
+                    if (config.nginx.http.upstream[i].server[v]._value == server) {
+                        callback(new Error(server + ' already exists in upstream pool ' + stream))
+                        return
+                    }
+                }
+                // if it doesn't exist, then push it to the config
+                config.nginx.http.upstream[i]._add('server', server)
+                log('Added server ' + server + ' to upsream pool ' + stream, '100', ' ', function (err) { if (err) { console.log(err)}})
+                config.flush()
+                callback(null)
+                return
+            }
+        }
+        callback(new Error(stream + ' does not exist!'))
+    })
+}
+
+var pool_delete = function (stream, server, callback) {
+    NginxConfFile.create(gc.global.nginx_conf, function (err, config) {
+        if (err) {
+            log('Error in pool_delete', '201', err, function (err) { if (err) { console.log(err)}})
+            console.log(err)
+            callback(err)
+        }
+        for (i=0; i < config.nginx.http.upstream.length; i++) {
+            if (config.nginx.http.upstream[i]._value == stream) {
+                // check to see if its already in the stream
+                for (v = 0; v < config.nginx.http.upstream[i].server.length; v++) {
+                    if (config.nginx.http.upstream[i].server[v]._value == server) {
+                        config.nginx.http.upstream[i]._remove('server', v)
+                        config.flush()
+                        callback(null)
+                        return
+                    }
+                }
+                // if it doesn't exist, then push it to the config
+                callback(new Error(server + " doesn't exist in upstream pool " + stream))
+            }
+        }
+    })
+}
+
+var pool_list = function (stream, callback) {
+    NginxConfFile.create(gc.global.nginx_conf, function (err, config) {
+        if (err) {
+            log('Error in pool_list', '203', err, function (err) { if (err) { console.log(err)}})
+            console.log(err)
+            callback(err)
+        }
+        for (i=0; i < config.nginx.http.upstream.length; i++) {
+            if (config.nginx.http.upstream[i]._value == stream) {
+                pool = {}
+                pool.members = []
+                pool.upstream = config.nginx.http.upstream[i]._value
+                try {
+                    if (config.nginx.http.upstream[i].server.length != undefined) {
+                        for (v = 0; v < config.nginx.http.upstream[i].server.length; v++) {
+                                pool.members.push(config.nginx.http.upstream[i].server[v]._value)
+                        }
+                    } else {
+                        pool.members.push(config.nginx.http.upstream[i].server._value)
+                    }
+                } catch (e) {
+                    // return empty array because otherwise it fails. this is SO HACKISH but IDK of any other way to get around the .length being 'undefined' when its <= 1
+                }
+                callback(null, pool)
+                return
+            }
+        }
+        callback(new Error(stream + ' does not exist!'))
+        return
+    })
+}
+////////////////////////////////////////////////////////// PUBLIC API
+
+app.get('/api', function (req, res) {
+    res.send('SERVER IS UP')
+})
+
+app.get('/config/engine', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            res.send(gc)
+        }
+    })
+})
+
+app.get('/config/nginx', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            get_nginx_config( function (err, config) {
+                if (err) {
+                    _err(req, err, function (err, msg) {
+                        res.send(msg)
+                    })
+                } else {
+                    res.send(config)
+                }
+            })
+        }
+    })
+})
+
+app.get('/config/log', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            logg = fs.readFileSync(gc.engined.logpath, 'utf-8')
+            res.send(logg)
+        }
+    })
+})
+
+app.get('/pool/add', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            pool_add(req.query.upstream, req.query.server, function (err) {
+                if (err) {
+                    _err(req, err, function (err, msg) {
+                        res.send(msg)
+                    })
+                } else {
+                    if (req.query.robot == undefined) {
+                        res.send('[OK] Engine added ' + req.query.server + ' to the pool ' + req.query.upstream)
+                    } else {
+                        res.send(200)
+                    }
+                }
+            })
+        }
+    })
+})
+
+app.get('/pool/delete', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            pool_delete (req.query.upstream, req.query.server, function (err) {
+                if (err) {
+                    _err(req, err, function (err, msg) {
+                        res.send(msg)
+                    })
+                } else {
+                    if (req.query.robot == undefined) {
+                        res.send('[OK] Engine deleted ' + req.query.server + ' from the pool ' + req.query.upstream)
+                    } else {
+                        res.send(200)
+                    }
+                }
+            })
+        }
+    })
+})
+
+app.get('/pool/list', function (req, res) {
+    check_auth_key(req.query.key, function (err) {
+        if (err) {
+            _err(req, err, function (err, msg) {
+                res.send(msg)
+            })
+        } else {
+            pool_list (req.query.upstream, function (err, data) {
+                if (err) {
+                    _err(req, err, function (err, msg) {
+                        res.send(msg)
+                    })
+                } else {
+                    if (req.query.robot == undefined) {
+                        res.send(data)
+                    } else {
+                        res.sendStatus(200)
+                    }
+                }
+            })
+        }
+    })
+})
+
+firstrun (function (err) {
+    if (err) {
+        log('Error in app startup', '500', err, function (err) { if (err) { console.log(err)}})
+        console.log(err)
+    } else {
+        app.listen(gc.global.api_port);
+        console.log('API listening on port ' + gc.global.api_port);
+    }
+})
